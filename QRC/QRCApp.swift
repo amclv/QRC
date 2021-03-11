95e9abd84c42110a8227a93985ddd247bbeb1107
Aaron Cleveland <toscleveland@gmail.com>
2021-03-11 02:15:06 -0600

    Initial Commit

---
+++ QRC/QRCApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCApp.swift
+//  QRC
+//
+//  Created by Aaron Cleveland on 3/11/21.
+//
+
+import SwiftUI
+
+@main
+struct QRCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
