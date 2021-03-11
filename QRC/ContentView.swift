752ef6ae680097403318b90e37c536dafa619c89
Aaron Cleveland <toscleveland@gmail.com>
2021-03-11 02:44:03 -0600

    Init Project, built small QRC in SwiftUI

--- QRC/ContentView.swift
+++ QRC/ContentView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            VStack {
+                QRCodeView(url: "kennydubroff.com")
+            }
+        }
     }
 }
 
